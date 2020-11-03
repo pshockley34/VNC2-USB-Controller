d6c3ba18efed3cc753587177b78b857d90eb4287
pshockley34 <pshockley34@gmail.com>
2020-11-03 14:24:44 -0600

    Update Mailbox.h

--- Mailbox.h
+++ Mailbox.h
@@ -16,18 +16,18 @@
 /* FTDI:EHF */
 
 /* FTDI:SDC Driver Constants */
-#define VOS_DEV_USBHOST_2		    0
-#define VOS_DEV_FAT_FILE_SYSTEM 1
-#define VOS_DEV_BOMS			      2
-#define VOS_DEV_UART			      3
+#define VOS_DEV_USBHOST_2           0
+#define VOS_DEV_FAT_FILE_SYSTEM     1
+#define VOS_DEV_BOMS                2
+#define VOS_DEV_UART                3
 
-#define VOS_NUMBER_DEVICES		  4
+#define VOS_NUMBER_DEVICES          4
 /* FTDI:EDC */
 
 /* FTDI:SDH Driver Handles */
-VOS_HANDLE hUSBHOST_2;           // USB Host Port 2
-VOS_HANDLE hFAT_FILE_SYSTEM;     // FAT File System for FAT32 and FAT16
-VOS_HANDLE hBOMS;                // Bulk Only Mass Storage for USB disks
-VOS_HANDLE hUART;					       // UART Driver Handle
+VOS_HANDLE hUSBHOST_2;              // USB Host Port 2
+VOS_HANDLE hFAT_FILE_SYSTEM;        // FAT File System for FAT32 and FAT16
+VOS_HANDLE hBOMS;                   // Bulk Only Mass Storage for USB disks
+VOS_HANDLE hUART;                   // UART Driver Handle
 /* FTDI:EDH */
 
